a2993b4661a58a1dc0e4e34a13419d83f98eed49
beavisruspb <beavisold@gmail.com>
2021-08-12 12:30:13 +0300

    all

--- main_v2.py
+++ main_v2.py
@@ -51,30 +51,35 @@ class MyShip:
                 self.live = True
 
 
-#создаем ячейки игрового поля
-a1 = MyCell()
-a2 = MyCell()
-a3 = MyCell()
+def main():
+    #создаем ячейки игрового поля
+    a1 = MyCell()
+    a2 = MyCell()
+    a3 = MyCell()
 
-#создаем корабли определяя список палуб для корабля
-ship1 = MyShip([a1, a2, a3])
+    #создаем корабли определяя список палуб для корабля
+    ship1 = MyShip([a1, a2, a3])
 
-print("кораблик", vars(ship1))
+    print("кораблик", vars(ship1))
 
-a1.check()
+    a1.check()
 
-print(vars(a1))
+    print(vars(a1))
 
-a3.check()
+    a3.check()
 
-print(vars(a3))
+    print(vars(a3))
 
-a2.check()
+    a2.check()
 
-print(vars(a2))
+    print(vars(a2))
 
-#таким образом коробль оживить нельзя ведь ячейки уже битые
-#необходимо либо оживить ячейки, либо указать нестреляные
-#ship1.__init__([a2, a1, a3])
-#print(vars(ship1))
+    print("Кораблик приплыл", vars(ship1))
 
+    #таким образом коробль оживить нельзя ведь ячейки уже битые
+    #необходимо либо оживить ячейки, либо указать нестреляные
+    #ship1.__init__([a2, a1, a3])
+    #print(vars(ship1))
+
+if __name__ == "__main__":
+    main()
