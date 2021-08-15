5e123309ae5e16c764a3dd26b686d768ffc4f3e3
beavisruspb <beavisold@gmail.com>
2021-08-15 19:47:46 +0300

    all

--- classes/field.py
+++ classes/field.py
@@ -1,23 +1,51 @@
 import ship
 import cell
+import random
+
+from string import ascii_lowercase
+import itertools
+
 class Field:
 
     cells = None
 
     ships = None
 
-    #возвращает количество живых кораблей
-    def shipAlive(self):
+    blockCells = None
+
+    def __init__(self, width, height):
+        self.makeField(width, height)
+
+        return
+
+    # На каждые 100 ячеек:
+    #    1 - 4хп
+    #    2 - 3хп
+    #    3 - 2хп
+    #    4 - 1оп
+    def placeShips(self, width, height):
+
+        #Свободная область для корабля
+        def findFreeCell(shipLong):
+                
+                while True:
+                    direct = random.choise(["up","down","right","left"])
+                    #ищем ключ
+                    startPosW = random.choice(list(self.cells.keys()))
+                    #ищем индекс
+                    startPosH = self.cells[startPosW].index(random.choise(self.cells[startPosW]))
+
+                    if direct == "up" and startPosH > shipLong:
+                        break
+                    if direct == "down" and startPosH + shipLong < len(self.cells[startPosW]):
+                        break
+                    if direct == "right" and 
+
+
+        cellsCount = width * height
+        factor = cellsCount // 100
+
         
-        shipCount = 0
-        #перебираем все шипы
-        for ship in ships:
-            if ship:
-                shipCount += 1
-        return shipCount
-
-    def placeShips(self):
-
         return
 
     #Создаем игровое поле
@@ -26,9 +54,6 @@ class Field:
         #Приватная служебная функция для правильного составления буквенных индексов
         #Возвращает list стрингов
         def __getWidhtIndex(width):
-
-            from string import ascii_lowercase
-            import itertools
 
             ss = list()
 
@@ -54,8 +79,16 @@ class Field:
                 bb.append(icell)
             self.cells.update( { a : bb } )
 
+    #возвращает количество живых кораблей
+    def shipsAlive(self):
+        
+        shipCount = 0
+        #перебираем все шипы
+        for ship in ships:
+            if ship:
+                shipCount += 1
+        return shipCount
 
 if __name__ == "__main__":
-    field = Field()
-    field.makeField(63,4)
+    field = Field(10,10)
     print(vars(field))
--- classes/ship.py
+++ classes/ship.py
@@ -3,14 +3,6 @@ class Ship:
     #лист палуб
     cells = []
     
-    def __init__(self,cells):
-        #запоминаем палубы
-        self.cells = cells
-        #перебираем палубы с цикле
-        for cell in self.cells:
-            #для каждой палубы предствляемся повелителем
-            cell.setShip(self)
-
     def isAlive(self):
         #посмотрим все ячейки корабля
         for cell in self.cells:
@@ -23,3 +15,6 @@ class Ship:
         
     def getSize(self):
         return len(self.cells)
+
+    def setCells(self, cells):
+        self.cells = cells
