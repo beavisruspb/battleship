2d9d0c01278123d6c22e3fc1cd63868a137d1d6e
Beavis <36653878+beavisruspb@users.noreply.github.com>
2021-08-23 11:00:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from classes.game import Game
+conf = {
+    "real_player_num" : 1,
+    "width" : 10,
+    "height" : 10,
+    ai_players : ['easy']
+}
+
 
 def main():
     game = Game()
-    game.start()
+    game.start(real_player_num, width, height, ai_players = ['easy'])
     return
 
 if __name__ == "__main__":
